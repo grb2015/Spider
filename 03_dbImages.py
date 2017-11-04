2a10bff15a07c3677de0f02cf49c481a2d3c28f0
guorenbin <949409706@qq.com>
2017-11-04 14:55:20 +0800

    renbin.guo add comment for 03.py

--- 03_dbImages.py
+++ 03_dbImages.py
@@ -8,7 +8,7 @@
 import urllib.request,socket,re,sys,os
 
 #定义文件保存路径
-targetPath = "E:\\projects\\Spider\\03_dbImages"
+targetPath = "./03_dbImages/"
 
 def saveFile(path):
     #检测当前路径的有效性
@@ -35,9 +35,36 @@ res = urllib.request.urlopen(req)
 
 data = res.read()
 
-for link,t in set(re.findall(r'(https:[^s]*?(jpg|png|gif))', str(data))):
 
+## renbin.guo added 2017-11-04  begin
+data = data.decode('utf-8')
+with open("./03_image.html",'w') as f:
+    f.write(data)
+
+## renbin.guo added 2017-11-04  end
+
+##  renbin.guo add comment  2017-11-04
+#   关于这里的正则表达式：
+#   1.原文中是[^s] 这里感觉用错了,需要转义\s
+#   2.(r'(https:[^\s]*?(jpg|png|gif))'的意义:
+#       它会匹配以https:开头 ,中间不含空格, 且以jpg,png,gif 结尾的字符串(url)
+#       a.这里[^\s]表示https: ~ jpg直接不能有空格,[^\s]*表示https:~jpg之间有任意多个非空格字符。可以用.*代替[^\s]*试试，这样的就会包含空格的url，
+#       b.这里的?表示惰性匹配，如果不加它，则jpg也可能被[^\s]匹配。可以去掉看看效果
+#       
+#
+#
+#
+#   3.这里有两个圆括号,第一个圆括号得到的一个变量即link,第二个圆括号(jpg|png|gif)得到一个图片类型的变量t 
+#                                                     但是如果你说不想要图片类型,不要第二组括号可以吗?即:(https:[^\s]*?jpg|png|gif )'                                                               
+#                                                     因为它是一个集合表达式,正则的语法决定了一定要。所以这里的圆括号是一举两用了,而且
+#                                                     不得不收集第二个参数。
+#   
+#                                                       
+#   
+###  renbin.guo end 
+for link,t in set(re.findall(r'(https:[^\s]*?(jpg|png|gif))', str(data))):
     print(link)
+#    print(t)
     try:
         urllib.request.urlretrieve(link,saveFile(link))
     except:
